167a13bdb541d3658e3b39731aa72a4181bf7436
Katsumi Kusumi <katsumikusumi@KatsuminoMacBook-Pro.local>
2018-11-07 22:21:05 +0900

    Complete Part11. And debug.

--- app/src/main/java/com/example/katsumikusumi/instagramcloneapp/Profile/ProfileActivity.java
+++ app/src/main/java/com/example/katsumikusumi/instagramcloneapp/Profile/ProfileActivity.java
@@ -1,5 +1,6 @@
 package com.example.katsumikusumi.instagramcloneapp.Profile;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -7,6 +8,8 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.example.katsumikusumi.instagramcloneapp.R;
 import com.example.katsumikusumi.instagramcloneapp.Utils.BottomNavigationViewHelper;
@@ -28,21 +31,16 @@ public class ProfileActivity extends AppCompatActivity{
     }
 
     private  void setupToolbar(){
-        Toolbar toolbar = (Toolbar) findViewById(R.id.profileToolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.profileToolBar);
         setSupportActionBar(toolbar);
 
-        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+        ImageView profileMenu = (ImageView) findViewById(R.id.profileMenu);
+        profileMenu.setOnClickListener(new View.OnClickListener() {
             @Override
-            public boolean onMenuItemClick(MenuItem item) {
-                Log.d(TAG, "onMenuItemClick: clicked menu item " + item);
-
-                switch(item.getItemId()){
-                    case R.id.profileMenu:
-                        Log.d(TAG, "onMenuItemClick: Navigationg to Profile Preferences");
-                        
-                }
-
-                return false;
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: navigating to accountsettings");
+                Intent intent = new Intent(mContext, AccountSettingsActivity.class);
+                startActivity(intent);
             }
         });
     }
@@ -60,9 +58,4 @@ public class ProfileActivity extends AppCompatActivity{
         menuItem.setChecked(true);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.profile_menu, menu);
-        return true;
-    }
 }
