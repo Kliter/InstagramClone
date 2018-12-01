85aafa2b9559f5898f8e22bf27a7beef40fd112d
Katsumi Kusumi <katsumi.kusumi.ph@gmail.com>
2018-12-01 11:57:19 +0900

    Complete Part31

--- app/src/main/java/com/example/katsumikusumi/instagramcloneapp/Profile/ProfileActivity.java
+++ app/src/main/java/com/example/katsumikusumi/instagramcloneapp/Profile/ProfileActivity.java
@@ -3,6 +3,8 @@ package com.example.katsumikusumi.instagramcloneapp.Profile;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentTabHost;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -35,84 +37,94 @@ public class ProfileActivity extends AppCompatActivity{
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
-
         Log.d(TAG, "onCreate: started.");
-        mProgressBar = (ProgressBar) findViewById(R.id.profileProgressBar);
-        mProgressBar.setVisibility(View.GONE);
 
-        setupBottomNavigationView();
-        setupToolbar();
-        setupActivityWidgets();
-        setProfileImage();
+        init();
 
-        tempGridSetup();
+//        setupBottomNavigationView();
+//        setupToolbar();
+//        setupActivityWidgets();
+//        setProfileImage();
+//
+//        tempGridSetup();
     }
+    
+    private void init (){
+        Log.d(TAG, "init: inflating" + getString(R.string.profile_fragment));
 
-    private void tempGridSetup(){
-        ArrayList<String> imgURLs = new ArrayList<>();
-        imgURLs.add("https://weekly.ascii.jp/elem/000/000/019/19847/android_limg_280x.jpg");
-        imgURLs.add("https://weekly.ascii.jp/elem/000/000/019/19847/android_limg_280x.jpg");
-        imgURLs.add("https://weekly.ascii.jp/elem/000/000/019/19847/android_limg_280x.jpg");
-        imgURLs.add("https://weekly.ascii.jp/elem/000/000/019/19847/android_limg_280x.jpg");
-        imgURLs.add("https://weekly.ascii.jp/elem/000/000/019/19847/android_limg_280x.jpg");
-        imgURLs.add("https://weekly.ascii.jp/elem/000/000/019/19847/android_limg_280x.jpg");
-        imgURLs.add("https://weekly.ascii.jp/elem/000/000/019/19847/android_limg_280x.jpg");
-        imgURLs.add("https://weekly.ascii.jp/elem/000/000/019/19847/android_limg_280x.jpg");
-        imgURLs.add("https://weekly.ascii.jp/elem/000/000/019/19847/android_limg_280x.jpg");
-        imgURLs.add("https://weekly.ascii.jp/elem/000/000/019/19847/android_limg_280x.jpg");
-        setupImageGrid(imgURLs);
-
+        ProfileFragment fragment = new ProfileFragment();
+        FragmentTransaction transaction = ProfileActivity.this.getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.container, fragment);
+        transaction.addToBackStack(getString(R.string.profile_fragment));
+        transaction.commit();
     }
+    
 
-    private void setupImageGrid(ArrayList<String> imgURLs){
-        GridView gridView = findViewById(R.id.gridView);
-
-        int gridWidth = getResources().getDisplayMetrics().widthPixels;
-        int imageWidth = gridWidth/NUM_GRID_COLUMNS;
-        gridView.setColumnWidth(imageWidth);
-
-        GridÎmageAdapter adapter = new GridÎmageAdapter(mContext, R.layout.layout_grid_imageview, "", imgURLs);
-        gridView.setAdapter(adapter);
-    }
-
-    private void setProfileImage(){
-        Log.d(TAG, "setProfileImage: setting profile photo.");
-        String imgURL = "https://weekly.ascii.jp/elem/000/000/019/19847/android_limg_280x.jpg";
-        UniversalImageLoader.setImage(imgURL, profilePhoto, mProgressBar, "https://");
-    }
-
-    private void setupActivityWidgets(){
-        mProgressBar = (ProgressBar) findViewById(R.id.profileProgressBar);
-        mProgressBar.setVisibility(View.GONE);
-        profilePhoto = (ImageView) findViewById(R.id.profile_photo);
-    }
-
-    private  void setupToolbar(){
-        Toolbar toolbar = (Toolbar) findViewById(R.id.profileToolBar);
-        setSupportActionBar(toolbar);
-
-        ImageView profileMenu = (ImageView) findViewById(R.id.profileMenu);
-        profileMenu.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Log.d(TAG, "onClick: navigating to accountsettings");
-                Intent intent = new Intent(mContext, AccountSettingsActivity.class);
-                startActivity(intent);
-            }
-        });
-    }
-
-    /**
-     * BottomNavigationView setup
-     */
-    private void setupBottomNavigationView(){
-        Log.d(TAG, "setupBottomNavigationView: setting up BottomNavigationView");
-        BottomNavigationViewEx bottomNavigationViewEx = (BottomNavigationViewEx) findViewById(R.id.bottomNavViewBar);
-        BottomNavigationViewHelper.setupBottomNavigationView(bottomNavigationViewEx);
-        BottomNavigationViewHelper.enableNavigation(mContext, bottomNavigationViewEx);
-        Menu menu = bottomNavigationViewEx.getMenu();
-        MenuItem menuItem = menu.getItem(ACTIVITY_NUM);
-        menuItem.setChecked(true);
-    }
+//    private void tempGridSetup(){
+//        ArrayList<String> imgURLs = new ArrayList<>();
+//        imgURLs.add("https://weekly.ascii.jp/elem/000/000/019/19847/android_limg_280x.jpg");
+//        imgURLs.add("https://weekly.ascii.jp/elem/000/000/019/19847/android_limg_280x.jpg");
+//        imgURLs.add("https://weekly.ascii.jp/elem/000/000/019/19847/android_limg_280x.jpg");
+//        imgURLs.add("https://weekly.ascii.jp/elem/000/000/019/19847/android_limg_280x.jpg");
+//        imgURLs.add("https://weekly.ascii.jp/elem/000/000/019/19847/android_limg_280x.jpg");
+//        imgURLs.add("https://weekly.ascii.jp/elem/000/000/019/19847/android_limg_280x.jpg");
+//        imgURLs.add("https://weekly.ascii.jp/elem/000/000/019/19847/android_limg_280x.jpg");
+//        imgURLs.add("https://weekly.ascii.jp/elem/000/000/019/19847/android_limg_280x.jpg");
+//        imgURLs.add("https://weekly.ascii.jp/elem/000/000/019/19847/android_limg_280x.jpg");
+//        imgURLs.add("https://weekly.ascii.jp/elem/000/000/019/19847/android_limg_280x.jpg");
+//        setupImageGrid(imgURLs);
+//
+//    }
+//
+//    private void setupImageGrid(ArrayList<String> imgURLs){
+//        GridView gridView = findViewById(R.id.gridView);
+//
+//        int gridWidth = getResources().getDisplayMetrics().widthPixels;
+//        int imageWidth = gridWidth/NUM_GRID_COLUMNS;
+//        gridView.setColumnWidth(imageWidth);
+//
+//        GridÎmageAdapter adapter = new GridÎmageAdapter(mContext, R.layout.layout_grid_imageview, "", imgURLs);
+//        gridView.setAdapter(adapter);
+//    }
+//
+//    private void setProfileImage(){
+//        Log.d(TAG, "setProfileImage: setting profile photo.");
+//        String imgURL = "https://weekly.ascii.jp/elem/000/000/019/19847/android_limg_280x.jpg";
+//        UniversalImageLoader.setImage(imgURL, profilePhoto, mProgressBar, "https://");
+//    }
+//
+//    private void setupActivityWidgets(){
+//        mProgressBar = (ProgressBar) findViewById(R.id.profileProgressBar);
+//        mProgressBar.setVisibility(View.GONE);
+//        profilePhoto = (ImageView) findViewById(R.id.profile_photo);
+//    }
+//
+//    private  void setupToolbar(){
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.profileToolBar);
+//        setSupportActionBar(toolbar);
+//
+//        ImageView profileMenu = (ImageView) findViewById(R.id.profileMenu);
+//        profileMenu.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Log.d(TAG, "onClick: navigating to accountsettings");
+//                Intent intent = new Intent(mContext, AccountSettingsActivity.class);
+//                startActivity(intent);
+//            }
+//        });
+//    }
+//
+//    /**
+//     * BottomNavigationView setup
+//     */
+//    private void setupBottomNavigationView(){
+//        Log.d(TAG, "setupBottomNavigationView: setting up BottomNavigationView");
+//        BottomNavigationViewEx bottomNavigationViewEx = (BottomNavigationViewEx) findViewById(R.id.bottomNavViewBar);
+//        BottomNavigationViewHelper.setupBottomNavigationView(bottomNavigationViewEx);
+//        BottomNavigationViewHelper.enableNavigation(mContext, bottomNavigationViewEx);
+//        Menu menu = bottomNavigationViewEx.getMenu();
+//        MenuItem menuItem = menu.getItem(ACTIVITY_NUM);
+//        menuItem.setChecked(true);
+//    }
 
 }
