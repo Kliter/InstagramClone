dc45ac73eb7c482095f13abb3ef22125059bad90
Katsumi Kusumi <katsumi.kusumi.ph@gmail.com>
2018-12-31 16:07:00 +0900

    Complete Part46.

---
+++ app/src/main/java/com/example/katsumikusumi/instagramcloneapp/Utils/FilePaths.java
@@ -0,0 +1,12 @@
+package com.example.katsumikusumi.instagramcloneapp.Utils;
+
+import android.os.Environment;
+
+public class FilePaths {
+
+    //"strage/emulated/0"
+    public String ROOT_DIR = Environment.getExternalStorageDirectory().getPath();
+
+    public String PICTURES = ROOT_DIR + "/Pictures";
+    public String CAMERA = ROOT_DIR + "/DCIM/camera";
+}
